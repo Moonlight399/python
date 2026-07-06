a803e6ca29dbd8da8680d01a98239eddcd9e7299
Moonlight399 <moonlightcrystals109@gmail.com>
2026-07-06 12:18:12 +0000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+
+
+#student_name, age, height, course ="Moon", "7","5.9","science"
+#print(student_name + " is" + " "+ age  +" "+"years"+ "and"+ " "+ height +"." + "He does" + " "+course )
+
+
+'''first_name= "quiver"
+surname= " string"
+full_name= first_name+surname
+age=int("56")
+print(full_name)
+print(age)'''
+
+
+
+red="255"
+blue="255"
+green="255"
+#comment
+white=int(red)+int(blue)+int(green)
+print(blue)
+print(green)
+print(red)
+print(white)
